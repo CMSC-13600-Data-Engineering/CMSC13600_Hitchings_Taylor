d817d3931f652fe2dcfb2e05ad07290209deb58d
Ashley Hitchings <72634325+ashleyhitchings@users.noreply.github.com>
2023-03-23 18:30:51 -0500

    Update views.py

--- attendancechimp/app/views.py
+++ attendancechimp/app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+import calendar
 
 
 def index(request):
     now = datetime.now()
-    month = now.ToString("MMMM")
+    month = now.strftime("%B")
     day = now.strftime("%d")
     time_str = 'Today is the ' + day + 'th of ' + month
     return render(request, 'app/index.html', {'dict':time_str})
