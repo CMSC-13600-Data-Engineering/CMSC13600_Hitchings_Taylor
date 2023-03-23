2e5cf755a9cbbc2eefa12073607131b6c03ecd91
Ashley Hitchings <72634325+ashleyhitchings@users.noreply.github.com>
2023-03-23 18:10:43 -0500

    Update views.py

--- attendancechimp/app/views.py
+++ attendancechimp/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
 
 
 def index(request):
-    return render(request, 'app/index.html', {})
+    today = date.today()
+    return render(request, 'app/index.html', {'dict':today})
