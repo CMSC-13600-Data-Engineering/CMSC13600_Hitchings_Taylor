e321e85eef2ad1dad62d34a2af6ca979e7648919
njltaylor <99927382+njltaylor@users.noreply.github.com>
2023-03-23 17:56:12 -0500

    Update views.py

--- attendancechimp/app/views.py
+++ attendancechimp/app/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'app/index.html', {})
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
