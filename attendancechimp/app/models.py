6d736f2a4c5e497f881d8bd9a1a5fcd09ec149ca
njltaylor <99927382+njltaylor@users.noreply.github.com>
2023-03-27 23:06:50 -0500

    Add files via upload

--- attendancechimp/app/models.py
+++ attendancechimp/app/models.py
@@ -1,51 +1,52 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
 # Create your models here.
 
 #Create a table of all users (students and instructors)
-    #Defining the choices 'student' and 'instructor'
-type_choices= (
-    ('Student','Student'),
-    ('Instructor','Instructor')
-)
+
 class User(models.Model):
-    userid=models.IntegerField(validators=[MaxLengthValidator(8),MinLengthValidator(8)],
-                           primary_key=True)
+    userid=models.IntegerField(primary_key=True, max_length=8)
     first_name=models.CharField(max_length=20,null=True,blank=True)
     last_name=models.CharField(max_length=20, null=True, blank=True)
-    user_class=models.CharField(
-        max_length=20, choices=type_choices,default='Student')
+    #Is a user a student or instructor
+    #When populating the table, need to ensure
+    user_class=models.CharField(max_length=20)
  
 #create a table of all courses
 class Courses(models.Model):
-    courseid=models.CharField(validators=[MaxLengthValidator(9),MinLengthValidator(9)],
-                              primary_key=True)
-    coursename=models.CharField(maxlength=75,null=True,blank=True)
-    instructor=models.ForeignKey(User,on_delete=models.CASCADE,
-                                 limit_choices_to={'user_class': 'Instructor'})
-    #Should the first 'User' be 'User.userid'?
+    courseid=models.CharField(max_length=9, primary_key=True)
+    coursename=models.CharField(max_length=75,null=True,blank=True)
+    instructor=models.ForeignKey(User,on_delete=models.CASCADE)
+#Must ensure in some way that students cannot accidentally be instructors
+    
 
 #Track what students are enrolled in what classes
 #This does allow a 'double enrollment' into a class
-#So code actuall populating the table needs to prevent that"""
+#So code actuall populating the table needs to prevent that
 class Enrollment(models.Model):
     enrollmentid=models.AutoField(primary_key=True)
-    student=models.ForeignKey(User,on_delete=models.CASCADE,
-                              limit_choices_to={'user_class':'Student'})
+    studentid=models.ForeignKey(User,on_delete=models.CASCADE)
     enrolledclass=models.ForeignKey(Courses, on_delete=models.CASCADE)
-    
+
 class Instructor_QRCodes(models.Model):
     qrid=models.AutoField(primary_key=True)
     course=models.ForeignKey(Courses,on_delete=models.CASCADE)
-    #qrcode=models.
-    #time=models.DateTimeField(args)
+    classmeeting=models.IntegerField()
+    qrcode=models.ImageField(upload_to='course_qr_codes')
+    time=models.DateTimeField()
+    
+class Attendance(models.Model):
+    attendanceid=models.AutoField(primary_key=True)
+    enrollmentid=models.ForeignKey(Enrollment,on_delete=models.CASCADE)
+    classmeeting=models.ForeignKey(Instructor_QRCodes, on_delete=models.CASCADE)
+    attended=models.BooleanField(default=False)
     
     
     
 class Uploaded_QRCodes(models.Model):
     uploadid=models.AutoField(primary_key=True)
-    qrcodeid=models.ForeignKey(Instructor_QRCodes,on_delete=models.CASCADE)
-    #qrcode=models.
-    #time=models.DateTimeField(args)
-    
-    
+    enrollmentid=models.ForeignKey(Enrollment,on_delete=models.CASCADE)
+    classmeeting=models.ForeignKey(Instructor_QRCodes,on_delete=models.CASCADE)
+    qrcode=models.ImageField(upload_to='uploaded_qr_codes')
+    time=models.DateTimeField()
+
+
