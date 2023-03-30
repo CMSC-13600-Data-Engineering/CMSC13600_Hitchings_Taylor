c05a26f5f99163208ef6a9508e8026cbc367dbe6
Ashley Hitchings <72634325+ashleyhitchings@users.noreply.github.com>
2023-03-30 11:37:42 -0500

    Update models.py nomenclature for clarity

--- attendancechimp/app/models.py
+++ attendancechimp/app/models.py
@@ -14,25 +14,24 @@ class User(models.Model):
 #create a table of all courses
 class Courses(models.Model):
     courseid=models.CharField(max_length=9, primary_key=True)
-    coursename=models.CharField(max_length=75,null=True,blank=True)
+    course_name=models.CharField(max_length=75,null=True,blank=True)
     instructor=models.ForeignKey(User,on_delete=models.CASCADE)
 #Must ensure in some way that students cannot accidentally be instructors
     
-
 #Track what students are enrolled in what classes
 #This does allow a 'double enrollment' into a class
 #So code actuall populating the table needs to prevent that
 class Enrollment(models.Model):
     enrollmentid=models.AutoField(primary_key=True)
-    studentid=models.ForeignKey(User,on_delete=models.CASCADE)
     enrolledclass=models.ForeignKey(Courses, on_delete=models.CASCADE)
+    studentid=models.ForeignKey(User,on_delete=models.CASCADE)
 
 class Instructor_QRCodes(models.Model):
     qrid=models.AutoField(primary_key=True)
-    course=models.ForeignKey(Courses,on_delete=models.CASCADE)
+    courseid=models.ForeignKey(Courses,on_delete=models.CASCADE)
     classmeeting=models.IntegerField()
     qrcode=models.ImageField(upload_to='course_qr_codes')
-    time=models.DateTimeField()
+    creation_time=models.DateTimeField()
     
 class Attendance(models.Model):
     attendanceid=models.AutoField(primary_key=True)
@@ -40,13 +39,9 @@ class Attendance(models.Model):
     classmeeting=models.ForeignKey(Instructor_QRCodes, on_delete=models.CASCADE)
     attended=models.BooleanField(default=False)
     
-    
-    
 class Uploaded_QRCodes(models.Model):
     uploadid=models.AutoField(primary_key=True)
     enrollmentid=models.ForeignKey(Enrollment,on_delete=models.CASCADE)
     classmeeting=models.ForeignKey(Instructor_QRCodes,on_delete=models.CASCADE)
-    qrcode=models.ImageField(upload_to='uploaded_qr_codes')
-    time=models.DateTimeField()
-
-
+    upload_qr=models.ImageField(upload_to='uploaded_qr_codes')
+    upload_time=models.DateTimeField()
